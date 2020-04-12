466dc6887d6f7ee6cb90f4dfce0ab29e069e37e3
leo <leok90@gmail.com>
2020-04-13 00:18:56 +0100

    bare minimum page styling

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,7 +2,6 @@ package mal8e
 
 import org.scalajs.dom
 import org.scalajs.dom.document
-import scala.scalajs.js.annotation.JSExportTopLevel
 
 import Mallacht._
 
@@ -13,6 +12,23 @@ object Main {
   //   parNode.textContent = text
   //   targetNode.appendChild(parNode)
   // }
+
+  def appendTextNode(targetNode: dom.Node, level: String, text: String): Unit = {
+    val newNode = document.createElement(level)
+    newNode.textContent = text
+    newNode.setAttribute("style", "text-align: center")
+
+    targetNode.appendChild(newNode)
+  }
+
+
+  def setHeaders(targetNode: dom.Node): Unit = {
+    appendTextNode(targetNode, "h1", "Mal8")
+    appendTextNode(targetNode, "h2", "Mal-Ocht")
+    appendTextNode(targetNode, "h3", "Mallacht")
+    appendTextNode(targetNode, "p", "Curse")
+  }
+
 
   def changePar(targetNode: dom.Node, text: String): Unit = {
     val parNode = document.getElementById("mallacht")
@@ -26,20 +42,24 @@ object Main {
 
   def setupUI(): Unit = {
 
+    val centrediv = document.createElement("div")
+    centrediv.setAttribute("id", "mydiv")
+    centrediv.setAttribute("style", "display: flex; flex-direction: column; justify-content: center; text-align: center;")
+
+    // setHeaders(document.getElementById("mydiv"))
+    setHeaders(document.body)
+
     val button = document.createElement("button")
     button.textContent = "Brúigh an cnaipe!"
+    button.setAttribute("class", "block")
 
     button.addEventListener("click", { (e: dom.MouseEvent) =>
       addClickedMessage()
     })
 
     val p = document.createElement("h2")
-    p.textContent = "Conas taoi inniú?"
+    p.textContent = "Fáilte"
     p.setAttribute("id", "mallacht")
-
-    val centrediv = document.createElement("div")
-    centrediv.setAttribute("id", "mydiv")
-    centrediv.setAttribute("align", "center")
 
     document.body.appendChild(centrediv)
     document.getElementById("mydiv").appendChild(button)
